400c45fcaeee27d9be1789e50ddc5debda78d406
brendanrichards73 <brendan.richards@btinternet.com>
2024-06-05 16:47:37 +0100

    added new portfolio exmaple from previous work

--- src/pages/Portfolio/Portfolio.tsx
+++ src/pages/Portfolio/Portfolio.tsx
@@ -30,6 +30,68 @@ export default function Portfolio() {
                             <div className={styles.infoitemContainer}>
                                 <div className={styles.infoContainer}>
                                     <h1 className={styles.description}>
+                                        Economist Core APP <br /> <br /> This is
+                                        not one of my personal projects but an
+                                        example of the work that I was part of
+                                        when employed by The Economist on thier
+                                        content squad. The app is build using
+                                        React-Native.
+                                    </h1>
+                                    <a
+                                        href="https://www.economist.com/"
+                                        target="_blank"
+                                        className={styles.gitHubLink}
+                                        rel="noreferrer"
+                                    >
+                                        Link to Economst.com
+                                    </a>
+                                </div>
+                                <div className={styles.itemContainer}>
+                                    <div
+                                        ref={item1Ref}
+                                        className={styles.item1}
+                                        onClick={handleClick(
+                                            "/videos/economist_clip.mov"
+                                        )}
+                                    >
+                                        <img
+                                            src="images/economist-app.jpg"
+                                            alt="economist app"
+                                        ></img>
+                                        <FaPlayCircle
+                                            className={styles.playIcon}
+                                        />
+                                        {isModalOpen &&
+                                            videoSource ===
+                                                "/videos/economist_clip.mov" && (
+                                                <div className={styles.modal}>
+                                                    <FaTimes
+                                                        className={
+                                                            styles.closeIcon
+                                                        }
+                                                        onClick={(e) => {
+                                                            e.stopPropagation();
+                                                            setIsModalOpen(
+                                                                false
+                                                            );
+                                                        }}
+                                                    />
+                                                    <video controls autoPlay>
+                                                        <source
+                                                            src={videoSource}
+                                                            type="video/mp4"
+                                                        />
+                                                        Your browser does not
+                                                        support the video tag.
+                                                    </video>
+                                                </div>
+                                            )}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.infoitemContainer}>
+                                <div className={styles.infoContainer}>
+                                    <h1 className={styles.description}>
                                         TODO APP <br /> <br /> A simple todo
                                         list app that allows users to add, and
                                         delete tasks. The app also allows users
@@ -95,13 +157,11 @@ export default function Portfolio() {
                                     <h1 className={styles.description}>
                                         Fitness App <br /> <br /> This is a
                                         project that I started in 2020, but it
-                                        wasn't worked on for the pasty few
-                                        years. Since then the original libraries
-                                        have become outdated so this is a new
-                                        project risen from the ashes! It is a
-                                        current ongoing project for a fitness
-                                        app. It is built using React-Native expo
-                                        cli and TypeScript.
+                                        wasn't worked on for the past few years.
+                                        I have recently picked this back up and
+                                        is a current ongoing project for a
+                                        fitness app. It is built using
+                                        React-Native expo cli and TypeScript.
                                     </h1>
                                     <a
                                         href="https://github.com/brendanrichards73/personal-trainer-2-2024"
